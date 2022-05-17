e2fa53f8e0eead140555a8a91cbd3bc97e1aca8c
Jerry Okolo <jerrycreative@live.co.uk>
2022-05-17 10:42:37 +0100

    helper functions for filter wrappedPunk sales

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { CToken } from "../generated/schema";
+import { BIGINT_ONE } from "./constant";
 
 export function getGlobalId(event: ethereum.Event): string {
   let globalId = event.transaction.hash
@@ -10,7 +11,7 @@ export function getGlobalId(event: ethereum.Event): string {
 }
 
 export function getOwnerFromCToken(event: ethereum.Event): string {
-  let cTokenLogIndex = event.logIndex.minus(BigInt.fromI32(1));
+  let cTokenLogIndex = event.logIndex.minus(BIGINT_ONE);
   let id = event.transaction.hash
     .toHexString()
     .concat("-")
@@ -32,6 +33,27 @@ export function getOwnerFromCToken(event: ethereum.Event): string {
   return owner;
 }
 
+export function getHashFromCToken(event: ethereum.Event): string {
+  let cTokenLogIndex = event.logIndex.minus(BIGINT_ONE);
+  let id = event.transaction.hash
+    .toHexString()
+    .concat("-")
+    .concat(cTokenLogIndex.toString());
+
+  let cToken = CToken.load(id);
+  if (!cToken) {
+    cToken = new CToken(id);
+    cToken.blockNumber = event.block.number;
+    cToken.referenceId = cToken.id;
+    cToken.blockHash = event.block.hash;
+    cToken.txHash = event.transaction.hash;
+    cToken.timestamp = event.block.timestamp;
+  }
+
+  let txHash = cToken.txHash.toHexString();
+
+  return txHash;
+}
 export function calculateAverage(totalAmount: BigInt, qty: BigInt): BigInt {
   let average = totalAmount.div(qty);
   return average;
