2cbc35059cddebc91e231fb3e084c5b0d3f53c41
Jerry Okolo <jerrycreative@live.co.uk>
2022-05-19 00:13:53 +0100

    new helper function to getPunk()

--- src/utils.ts
+++ src/utils.ts
@@ -58,6 +58,31 @@ export function getContractAddress(event: ethereum.Event): string | null {
   return contractAddress as string;
 }
 
+export function getPunkId(event: ethereum.Event): string | null {
+  //The transfer always come first, so we need to provide the correct logIndex for cToken
+  let cTokenLogIndex = event.logIndex.minus(BIGINT_ONE);
+
+  let id = event.transaction.hash
+    .toHexString()
+    .concat("-")
+    .concat(cTokenLogIndex.toString());
+
+  /**
+   * We only care about transactions concerning WrappedPunk contract
+   * cToken should exist with the given ID.
+   */
+  let cToken = CToken.load(id);
+
+  // if it doesn't then it's not a WrappedPunk transaction
+  if (!cToken) {
+    return null;
+  }
+
+  // if it does, then return the punkId to enable us validate the transaction in OrderMatched()
+  let punk = cToken.punkId;
+  return punk as string;
+}
+
 export function calculateAverage(totalAmount: BigInt, qty: BigInt): BigInt {
   let average = totalAmount.div(qty);
   return average;
