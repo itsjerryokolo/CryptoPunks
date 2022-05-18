8c45ecde5454b855389789c5d0214eeadf64b075
Jerry Okolo <jerrycreative@live.co.uk>
2022-05-19 00:12:56 +0100

    cleanup mapping files

--- src/mapping.ts
+++ src/mapping.ts
@@ -18,7 +18,6 @@ import {
 import { getTrait } from "./traits";
 
 import { Buy as ERC721Sale } from "../generated/ERC721Sale/ERC721Sale";
-import { Buy as RaribleExchangeV1Buy } from "../generated/RaribleExchangeV1/RaribleExchangeV1";
 
 import {
   Punk,
@@ -27,8 +26,6 @@ import {
   UserProxy,
   Ask,
   Bid,
-  CToken,
-  Contract,
 } from "../generated/schema";
 
 import {
@@ -37,6 +34,7 @@ import {
   BIGINT_ONE,
   BIGINT_ZERO,
 } from "./constant";
+
 import {
   getOrCreateAccount,
   createMetadata,
@@ -750,6 +748,7 @@ export function handleWrappedPunkTransfer(event: WrappedPunkTransfer): void {
     cToken.from = event.params.from.toHexString();
     cToken.to = event.params.to.toHexString();
     cToken.owner = event.params.to.toHexString();
+    cToken.punkId = event.params.tokenId.toString();
 
     //We need the contract address to filter our transactions from ERC721Sale(Rarible) Contract
     cToken.referenceId = event.address.toHexString();
@@ -782,144 +781,4 @@ export function handleProxyRegistered(event: ProxyRegistered): void {
   userProxy.blockNumber = event.block.number;
   userProxy.blockHash = event.block.hash;
   userProxy.save();
-}
-
-export function handleBuy(event: ERC721Sale): void {
-  //ERC721Sale Contract - Track WRAPPEDPUNK SALE
-  /**
-   * ROOT ISSUE:  Punk 4216 was sold while wrapped.
-        https://cryptopunks.app/cryptopunks/accountinfo?account=0x0c8e854729144ab6405939819f461764647f52ed
-
-          This is an example of a sale that occurs before a punk is unwrapped.
-            - https://etherscan.io/tx/0xae3fc4123415e985850f9d41dc162a84c0b6a976ead1deedecf0c2bad66685e2#eventlog
-      - We want to capture this so we can calculate average prices & update other aggregates both for punk & account
-  */
-
-  let wrappedPunkContractAddress = getContractAddress(event);
-
-  /**
-   * We filter out wrappedPunk transactions by ensuring
-   *    - both events occur in the same transaction
-   *    - it's the wrappedPunk contract address that emitted it
-   */
-  if (
-    wrappedPunkContractAddress !== null &&
-    wrappedPunkContractAddress == WRAPPED_PUNK_ADDRESS
-  ) {
-    let fromAccount = getOrCreateAccount(event.params.seller);
-    let toAccount = getOrCreateAccount(event.params.buyer);
-    let punk = Punk.load(event.params.tokenId.toString())!;
-    let sale = getOrCreateSale(
-      event.params.seller,
-      event.params.tokenId.toString(),
-      event
-    );
-
-    sale.amount = event.params.price;
-    sale.to = event.params.buyer.toHexString();
-
-    //Update fromAccount aggregates
-    fromAccount.numberOfSales = fromAccount.numberOfSales.plus(BIGINT_ONE);
-    fromAccount.totalEarned = fromAccount.totalEarned.plus(event.params.price);
-
-    //Update toAccount aggregates
-    toAccount.totalSpent = toAccount.totalSpent.plus(event.params.price);
-    toAccount.numberOfPurchases = toAccount.numberOfPurchases.plus(BIGINT_ONE);
-
-    //We only calculate average sale price if there are more than 0 sales so we don't divide by 0
-    if (toAccount.numberOfPurchases != BIGINT_ZERO) {
-      toAccount.averageAmountSpent = calculateAverage(
-        toAccount.totalSpent,
-        toAccount.numberOfPurchases
-      );
-    }
-
-    //Update punk aggregates
-    punk.totalAmountSpentOnPunk = punk.totalAmountSpentOnPunk.plus(
-      event.params.price
-    );
-
-    //We only calculate average sale price if there are more than 0 sales so we don't divide by 0
-    if (punk.numberOfSales != BIGINT_ZERO) {
-      punk.averageSalePrice = calculateAverage(
-        punk.totalAmountSpentOnPunk,
-        punk.numberOfSales
-      );
-    }
-    toAccount.save();
-    fromAccount.save();
-    sale.save();
-    punk.save();
-  }
-}
-
-export function handleExchangeV1Buy(event: RaribleExchangeV1Buy): void {
-  //RaribleExchangeV1 Contract - Track WRAPPEDPUNK SALE
-  /**
-   * ROOT ISSUE:  Punk 509 was sold while wrapped.
-        https://cryptopunks.app/cryptopunks/accountinfo?account=0x0eb9a7ff5cbf719251989caf1599c1270eafb531
-
-          example:
-            - https://etherscan.io/tx/0x51583622e0dcfda43c6481ba073eb1bbd6b7f3ef98c28d3564918491344d8ce3#eventlog
-      - We want to capture this so we can calculate average prices & update other aggregates both for punk & account
-  */
-
-  let wrappedPunkContractAddress = getContractAddress(event);
-
-  /**
-   * We filter out wrappedPunk transactions by ensuring
-   *    - both events occur in the same transaction
-   *    - it's the wrappedPunk contract address that emitted it
-   */
-  if (
-    wrappedPunkContractAddress !== null &&
-    wrappedPunkContractAddress == WRAPPED_PUNK_ADDRESS
-  ) {
-    let fromAccount = getOrCreateAccount(event.params.owner);
-    let toAccount = getOrCreateAccount(event.params.buyer);
-    let punk = Punk.load(event.params.buyTokenId.toString())!;
-    let sale = getOrCreateSale(
-      event.params.owner,
-      event.params.buyTokenId.toString(),
-      event
-    );
-
-    sale.amount = event.params.buyValue;
-    sale.to = event.params.buyer.toHexString();
-
-    //Update fromAccount aggregates
-    fromAccount.numberOfSales = fromAccount.numberOfSales.plus(BIGINT_ONE);
-    fromAccount.totalEarned = fromAccount.totalEarned.plus(
-      event.params.buyValue
-    );
-
-    //Update toAccount aggregates
-    toAccount.totalSpent = toAccount.totalSpent.plus(event.params.buyValue);
-    toAccount.numberOfPurchases = toAccount.numberOfPurchases.plus(BIGINT_ONE);
-
-    //We only calculate average sale price if there are more than 0 sales so we don't divide by 0
-    if (toAccount.numberOfPurchases != BIGINT_ZERO) {
-      toAccount.averageAmountSpent = calculateAverage(
-        toAccount.totalSpent,
-        toAccount.numberOfPurchases
-      );
-    }
-
-    //Update punk aggregates
-    punk.totalAmountSpentOnPunk = punk.totalAmountSpentOnPunk.plus(
-      event.params.buyValue
-    );
-
-    //We only calculate average sale price if there are more than 0 sales so we don't divide by 0
-    if (punk.numberOfSales != BIGINT_ZERO) {
-      punk.averageSalePrice = calculateAverage(
-        punk.totalAmountSpentOnPunk,
-        punk.numberOfSales
-      );
-    }
-    toAccount.save();
-    fromAccount.save();
-    sale.save();
-    punk.save();
-  }
 }
