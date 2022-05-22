aaf568310b02ccf375c8ea535f1608fda88fb818
Jerry Okolo <jerrycreative@live.co.uk>
2022-05-22 23:37:40 +0100

    remove WETH contract

--- src/WETHTransfer.ts
+++
@@ -1,7 +0,0 @@
-import { getOrCreateCToken } from "./helpers/entityHelper";
-
-export function handleTransfer(event: Transfer): void {
-  let cToken = getOrCreateCToken(event.params.tokenId);
-  cToken.owner = event.params.to;
-  cToken.save();
-}
