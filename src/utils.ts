8437ed7b089c9a1f68b3e893d716656a5c80844b
Jerry Okolo <jerrycreative@live.co.uk>
2022-05-31 08:37:10 +0100

    helper function to get price after Rarible fee cut

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { BigInt, ethereum, BigDecimal } from "@graphprotocol/graph-ts";
 import { CToken } from "../generated/schema";
 import { BIGINT_ONE } from "./constant";
 
@@ -107,6 +107,10 @@ export function getMakerAddress(event: ethereum.Event): string | null {
   return makerAddress as string;
 }
 
+/**
+ * @description This function calculates the average given two values
+ * @returns Returns the average in `BigInt`
+ */
 export function calculateAverage(totalAmount: BigInt, qty: BigInt): BigInt {
   let average = totalAmount.div(qty);
   return average;
@@ -123,4 +127,33 @@ export function getOrCreateCToken(event: ethereum.Event): CToken {
     cToken.timestamp = event.block.timestamp;
   }
   return cToken as CToken;
+}
+
+/**
+ * @description This function takes out the Rarible cut from the sale price
+ * @summary 
+     - We convert the percentage cut to a decimal
+     - We get the cut in ETH in BigDecimal
+     - We subtract the cut from the `price` to get the final sale price
+
+ * @param price - Sale amount
+ * @returns Returns the ETH Sale price in `BigInt` after cut conversion
+ */
+
+export function getPriceAfterRaribleCut(price: BigInt): BigInt {
+  let cut = BigDecimal.fromString("0.025"); //2.5%
+
+  let cutDecimal = cut.times(price.toBigDecimal());
+  let cutStringFromDecimal = cutDecimal.toString();
+
+  //Extra check incase the value ends with ".0", e.g 12.0
+  if (cutStringFromDecimal.includes(".") === true) {
+    let priceAfterCut = price.minus(
+      BigInt.fromString(cutStringFromDecimal.split(".")[0])
+    );
+    return priceAfterCut;
+  } else {
+    let priceAfterCut = price.minus(BigInt.fromString(cutStringFromDecimal));
+    return priceAfterCut;
+  }
 }
