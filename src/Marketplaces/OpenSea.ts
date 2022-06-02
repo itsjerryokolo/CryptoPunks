159bd6eae6b0ecf0423bb8e8e3d0fcb6371d3897
Jerry Okolo <jerrycreative@live.co.uk>
2022-06-02 11:29:09 +0100

    code cleanup

--- src/Marketplaces/OpenSea.ts
+++ src/Marketplaces/OpenSea.ts
@@ -16,18 +16,17 @@ import { updatePunkSaleAggregates } from '../../src/helpers/punkHelper'
 
 export function handleOpenSeaSale(event: OrdersMatched): void {
 	/**
-      @summary OpenSea Contract - Track WRAPPEDPUNK sale
-     * ROOT ISSUE:  Punk 7443 was sold on Opensea while wrapped.
-       Account: https://cryptopunks.app/cryptopunks/accountinfo?account=0x0eb9a7ff5cbf719251989caf1599c1270eafb531
+      @description OpenSea Contract - Track WRAPPEDPUNK sale
+	  @summary:
+    	 ROOT ISSUE:  Punk 7443 was sold on Opensea while wrapped.
+      		 Account: https://cryptopunks.app/cryptopunks/accountinfo?account=0x0eb9a7ff5cbf719251989caf1599c1270eafb531
         - Example:
             - https://etherscan.io/tx/0xac6acdca9aeb00238ff885dcd4e697afd1cfa8ba75ef69622f786b96f8d164cf#eventlog
         - We want to capture this so we can calculate average prices & update other aggregates both for punk & account
-    */
 
-	/**
-	 * We filter out wrappedPunk transactions by ensuring
-	 *    - both events occur in the same transaction
-	 *    - the wrappedPunk contract address emitted it
+		- We filter out wrappedPunk transactions by ensuring
+			- both events occur in the same transaction
+			- the wrappedPunk contract address emitted it
 	 */
 	let wrappedPunkContractAddress = getContractAddress(event)
 	if (
@@ -74,21 +73,18 @@ export function handleOpenSeaSale(event: OrdersMatched): void {
 				makerAddress !== null &&
 				makerAddress == event.params.maker.toHexString()
 			) {
-				/**
-         * @summary Bid accepted sale
-            * We want to capture this sale.
+		/**
+         * @description Logic for validating bidAccepted sale:
+		 * @summary 
+		  	- We want to capture this sale.
                 - The major difference between this sale and a regular sale is that
                     - the maker becomes the buyer --> (toAccount)
                     - the taker becomes the seller --> (fromAccount)
                 - Example:
                      https://etherscan.io/tx/0x0e44a5eb1d553ab2daacf43fd50bcd73f030e739de009368a9f2897150e1215d#eventlog
-         */
 
-				/** 
-         * @description Rationale for validating bidAccepted sale:
-             Getting the maker address from the toAccount in the wrappedPunk Transfer event confirms that
-             this is a bid accepted sale because the maker is the buyer,
-             but in the OrderMatched event, the maker is the seller.
+            - Getting the maker address from the toAccount in the wrappedPunk Transfer event confirms that
+              this is a bid accepted sale because the maker is the buyer, but in the OrderMatched event, the maker is the seller.
         */
 				let price = event.params.price
 				let seller = event.params.taker
