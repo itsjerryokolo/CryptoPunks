07615fa8f5ca5b77a261f9deaf791d9977fb75f1
Jerry Okolo <jerrycreative@live.co.uk>
2022-06-07 08:02:10 +0100

    Fix issue #48: nft id for transfer events

--- src/helpers/transferHelper.ts
+++ src/helpers/transferHelper.ts
@@ -1,29 +1,29 @@
-import { BigInt, ethereum } from "@graphprotocol/graph-ts";
-import { Transfer } from "../../generated/schema";
+import { BigInt, ethereum } from '@graphprotocol/graph-ts'
+import { Transfer } from '../../generated/schema'
 
-import { getGlobalId } from "../utils";
+import { getGlobalId } from '../utils'
 
 export function getOrCreateTransfer(
-  punk: BigInt | null,
-  event: ethereum.Event
+	punk: BigInt | null,
+	event: ethereum.Event
 ): Transfer {
-  let transfer = Transfer.load(getGlobalId(event).concat("-TRANSFER"));
-  if (!transfer) {
-    transfer = new Transfer(getGlobalId(event).concat("-TRANSFER"));
-  }
+	let transfer = Transfer.load(getGlobalId(event).concat('-TRANSFER'))
+	if (!transfer) {
+		transfer = new Transfer(getGlobalId(event).concat('-TRANSFER'))
+	}
 
-  //find out how they differ & where to find them
-  //transfer.from = fromAddress.toHexString();
-  //transfer.to = toAddress.toHexString();
-  //transfer.nft = punk.toString() ;// null by default
+	//find out how they differ & where to find them
+	//transfer.from = fromAddress.toHexString();
+	//transfer.to = toAddress.toHexString();
+	transfer.nft = punk.toString() // null by default
 
-  transfer.timestamp = event.block.timestamp;
-  transfer.contract = event.address.toHexString();
-  transfer.blockNumber = event.block.number;
-  transfer.txHash = event.transaction.hash;
-  transfer.blockHash = event.block.hash;
-  transfer.type = "TRANSFER";
+	transfer.timestamp = event.block.timestamp
+	transfer.contract = event.address.toHexString()
+	transfer.blockNumber = event.block.number
+	transfer.txHash = event.transaction.hash
+	transfer.blockHash = event.block.hash
+	transfer.type = 'TRANSFER'
 
-  transfer.save();
-  return transfer as Transfer;
+	transfer.save()
+	return transfer as Transfer
 }
