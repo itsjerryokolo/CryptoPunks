585c8cfe9f55d8e304a70d7e0767121850ef2d66
Jerry Okolo <jerrycreative@live.co.uk>
2022-06-09 07:35:46 +0100

    fix bug: logic to track accepted bid on RaribleV1

--- src/Marketplaces/RaribleExchangeV1.ts
+++ src/Marketplaces/RaribleExchangeV1.ts
@@ -1,7 +1,7 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { Buy as RaribleExchangeV1Buy } from '../../generated/RaribleExchangeV1/RaribleExchangeV1'
 import { Punk } from '../../generated/schema'
-import { WRAPPED_PUNK_ADDRESS, WETH_CONTRACT } from '../constant'
+import { WRAPPED_PUNK_ADDRESS } from '../constant'
 import { getOrCreateSale } from '../helpers/saleHelper'
 import {
 	getOrCreateAccount,
@@ -13,7 +13,11 @@ import {
 } from '../helpers/contractHelper'
 import { updatePunkSaleAggregates } from '../../src/helpers/punkHelper'
 import { updateSale } from '../helpers/saleHelper'
-import { getContractAddress, getPriceAfterRaribleCut } from '../utils'
+import {
+	getContractAddress,
+	getMakerAddress,
+	getPriceAfterRaribleCut,
+} from '../utils'
 
 export function handleExchangeV1Buy(event: RaribleExchangeV1Buy): void {
 	/**
@@ -21,7 +25,6 @@ export function handleExchangeV1Buy(event: RaribleExchangeV1Buy): void {
       @description 
 		ROOT ISSUE:  Punk 509 was sold while wrapped.
 			- Account: https://cryptopunks.app/cryptopunks/accountinfo?account=0x0eb9a7ff5cbf719251989caf1599c1270eafb531
-			- Example: https://etherscan.io/tx/0x51583622e0dcfda43c6481ba073eb1bbd6b7f3ef98c28d3564918491344d8ce3#eventlog
     	- We want to capture this so we can calculate average prices & update other aggregates both for punk & account
 		- We filter out wrappedPunk transactions by ensuring
 			- both events occur in the same transaction.
@@ -33,43 +36,24 @@ export function handleExchangeV1Buy(event: RaribleExchangeV1Buy): void {
 		wrappedPunkContractAddress !== null &&
 		wrappedPunkContractAddress == WRAPPED_PUNK_ADDRESS
 	) {
-		let sellToken = event.params.sellToken.toString()
-		let price = event.params.buyValue
-		let buyer = event.params.buyer
-		let seller = event.params.owner
-		let tokenId = event.params.buyTokenId.toString()
-
-		if (sellToken == WRAPPED_PUNK_ADDRESS) {
-			let contract = getOrCreateWrappedPunkContract(
-				Address.fromString(wrappedPunkContractAddress)
-			)
-			let fromAccount = getOrCreateAccount(seller)
-			let toAccount = getOrCreateAccount(buyer)
-			let punk = Punk.load(tokenId)!
-			let sale = getOrCreateSale(seller, tokenId, event)
-
-			updateSale(sale, price, buyer)
-			updateContractAggregates(contract, price)
-			updateAccountAggregates(fromAccount, toAccount, price)
-			updatePunkSaleAggregates(punk, price)
-
-			//Write
-			toAccount.save()
-			fromAccount.save()
-			sale.save()
-			contract.save()
-			punk.save()
-		} else if (sellToken == WETH_CONTRACT) {
+		let trueBuyer = getMakerAddress(event)
+		if (trueBuyer !== null && trueBuyer == event.params.owner.toHexString()) {
 			/**
 		    @summary Logic for validating a bidAccepted sale
 			@description 
 			A wrapped punk bid can be accepted on RaribleExchangeV1.
 				- Example: https://etherscan.io/tx/0x26ad41d72737442ef108460bc25a69764b30e3df96344d95f8f3a03a551fd787#eventlog
-				- We know this through the sell token.
-					- If the sell token is WETH, then we know it's a wrapped punk bid that got accepted
-					- If the sell token is the wrapped punk contract, then we know it's a regular sale
+				- We know this through the buyer address.
+                - The major difference between this sale and a regular sale is that
+                    - the owner address in Ordermatched becomes the buyer --> (toAccount)
+                    - the seller address becomes the buyer --> (fromAccount)
 				- RaribleExchangeV1 takes 2.5% fee on all bids accepted transactions so we need to remove that to get the actual sale price
 			*/
+			let price = event.params.buyValue
+			let buyer = event.params.owner
+			let seller = event.params.buyer
+			let tokenId = event.params.buyTokenId.toString()
+
 			let bidPrice = getPriceAfterRaribleCut(price)
 			let contract = getOrCreateWrappedPunkContract(
 				Address.fromString(wrappedPunkContractAddress)
@@ -83,6 +67,38 @@ export function handleExchangeV1Buy(event: RaribleExchangeV1Buy): void {
 			updateContractAggregates(contract, bidPrice)
 			updateAccountAggregates(fromAccount, toAccount, bidPrice)
 			updatePunkSaleAggregates(punk, bidPrice)
+
+			//Write
+			toAccount.save()
+			fromAccount.save()
+			sale.save()
+			contract.save()
+			punk.save()
+		} else if (
+			trueBuyer !== null &&
+			trueBuyer == event.params.buyer.toHexString()
+		) {
+			/**
+			 @summary - Logic for Regular Sale
+				- Example: https://etherscan.io/tx/0x51583622e0dcfda43c6481ba073eb1bbd6b7f3ef98c28d3564918491344d8ce3#eventlog
+			 */
+			let price = event.params.buyValue
+			let buyer = event.params.buyer
+			let seller = event.params.owner
+			let tokenId = event.params.buyTokenId.toString()
+
+			let contract = getOrCreateWrappedPunkContract(
+				Address.fromString(wrappedPunkContractAddress)
+			)
+			let fromAccount = getOrCreateAccount(seller)
+			let toAccount = getOrCreateAccount(buyer)
+			let punk = Punk.load(tokenId)!
+			let sale = getOrCreateSale(seller, tokenId, event)
+
+			updateSale(sale, price, buyer)
+			updateContractAggregates(contract, price)
+			updateAccountAggregates(fromAccount, toAccount, price)
+			updatePunkSaleAggregates(punk, price)
 
 			//Write
 			toAccount.save()
