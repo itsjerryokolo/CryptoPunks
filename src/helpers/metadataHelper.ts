888cc068dbc40735ba080829e742140fdcd50ff7
Jerry Okolo <jerrycreative@live.co.uk>
2022-06-17 08:30:12 +0100

    feat: punk svg images

--- src/helpers/metadataHelper.ts
+++ src/helpers/metadataHelper.ts
@@ -1,19 +1,20 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { CONTRACT_URI, IMAGE_URI, TOKEN_URI } from "../constant";
-import { MetaData } from "../../generated/schema";
+import { BigInt } from '@graphprotocol/graph-ts'
+import { CONTRACT_URI, IMAGE_URI, TOKEN_URI } from '../constant'
+import { MetaData } from '../../generated/schema'
+import { PunkImage } from '../punkSvg'
 
 export function createMetadata(punkId: BigInt): MetaData {
-  let metadata = new MetaData(punkId.toString());
-  metadata.tokenURI = TOKEN_URI.concat(punkId.toString());
-  metadata.contractURI = CONTRACT_URI;
-  metadata.tokenId = punkId;
-  metadata.punk = punkId.toString();
-  metadata.contractURI = CONTRACT_URI;
-  metadata.imageURI = IMAGE_URI.concat(punkId.toString()).concat(".png");
+	let metadata = new MetaData(punkId.toString())
+	metadata.tokenURI = TOKEN_URI.concat(punkId.toString())
+	metadata.contractURI = CONTRACT_URI
+	metadata.tokenId = punkId
+	metadata.punk = punkId.toString()
+	metadata.contractURI = CONTRACT_URI
+	metadata.image = PunkImage.get(punkId.toString())
 
-  metadata.traits = new Array<string>();
+	metadata.traits = new Array<string>()
 
-  metadata.save();
+	metadata.save()
 
-  return metadata as MetaData;
+	return metadata as MetaData
 }
---
+++ src/punkSvg.ts
@@ -0,0 +1,3 @@
+import { TypedMap } from '@graphprotocol/graph-ts'
+
+export let PunkImage = new TypedMap<string, string>()
