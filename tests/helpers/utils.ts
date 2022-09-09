f55978dfcd6b4eee092b36f6ee6ead1af0427375
Jerry Okolo <jerrycreative@live.co.uk>
2022-09-09 19:18:14 +0100

    first running test

--- tests/helpers/utils.ts
+++ tests/helpers/utils.ts
@@ -18,17 +18,28 @@ export default class Utils {
 		return '0x06b306c85e5f33b1b2d971822ce0ed42fb7ab9a1'
 	}
 
+	static get accountDummyOne(): Address {
+		return Address.fromHexString('0x06b306c85e5f33b1b2d971822ce0ed42fb7ab9a1')
+	}
+	static get accountDummyZero(): Address {
+		return Address.fromHexString('0x0000000000000000000000000000000000000000')
+	}
+
+	static get accountDummyZero_BYTES(): Bytes {
+		return Bytes.fromHexString('0x0000000000000000000000000000000000000000')
+	}
+
 	static get Bi_ZERO(): BigInt {
 		return BigInt.zero()
 	}
 
-	static seedBounty(): void {
+	static seedAccount(): void {
 		let entity = new Entity()
 
-		entity.setBytes('id', Utils.id_BYTES)
-		entity.setString('bountyAddress', Utils.id_STRING)
+		entity.setString('id', Utils.id_STRING)
+		entity.setBytes('address', Utils.id_BYTES)
 		entity.setBigInt('status', Utils.Bi_ZERO)
 
-		store.set('Entity', Utils.id_STRING, entity)
+		store.set('Account', Utils.id_STRING, entity)
 	}
 }
